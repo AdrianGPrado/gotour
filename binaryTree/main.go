bfab4655dc23dae1465b1b6a3540b724066fa59b
Adrian G Prado <Adrian.GarciaPrado@vacasa.com>
2019-02-22 22:15:47 -0800

    binary tree

--- binaryTree/main.go
+++ binaryTree/main.go
@@ -6,13 +6,55 @@ import (
 	"golang.org/x/tour/tree"
 )
 
-// type Tree struct {
-// 	right *Tree
-// 	value int
-// 	left  *Tree
-// }
+func walk(t *tree.Tree, ch chan int) {
+	if t == nil {
+		return
+	}
+	if t.Left != nil {
+		walk(t.Left, ch)
+	}
+	if t.Right != nil {
+		walk(t.Right, ch)
+	}
+	ch <- t.Value
+}
+
+func walker(t *tree.Tree, ch chan int) {
+	walk(t, ch)
+	close(ch)
+}
+
+func printTree(ch chan int) {
+	for i := range ch {
+		fmt.Printf("%v ", i)
+	}
+}
+
+func compareTrees(ch1, ch2 chan int) bool {
+	for i := range ch1 {
+		j := <-ch2
+		if i != j {
+			return false
+		}
+	}
+	return true
+}
 
 func main() {
+	ch1 := make(chan int)
 	t1 := tree.New(2)
-	fmt.Printf(t1)
+	go walker(t1, ch1)
+	t2 := tree.New(2)
+	ch2 := make(chan int)
+	go walker(t2, ch2)
+
+	fmt.Println("\ntree 1 ")
+	printTree(ch1)
+
+	fmt.Println("\ntree 2 ")
+	printTree(ch2)
+
+	equal := compareTrees(ch1, ch2)
+	fmt.Println("\nAre trees equal: ", equal)
+
 }
--- go.mod
+++ go.mod
@@ -1,1 +1,3 @@
 module github.com/AdrianGPrado/Concurrency
+
+require golang.org/x/tour v0.0.0-20190206171615-838ce51196a7
